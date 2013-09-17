9e6a5dba61b75701a40dc39676902470a3b76788
Chris Ballinger <chrisballinger@gmail.com>
2013-09-17 10:01:15 -0700

    Initial Commit

---
+++ FFmpegWrapper/FFmpegWrapper.h
@@ -0,0 +1,13 @@
+//
+//  FFmpegWrapper.h
+//  FFmpegWrapper
+//
+//  Created by Christopher Ballinger on 9/17/13.
+//  Copyright (c) 2013 OpenWatch, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FFmpegWrapper : NSObject
+
+@end
---
+++ FFmpegWrapper/FFmpegWrapper.m
@@ -0,0 +1,13 @@
+//
+//  FFmpegWrapper.m
+//  FFmpegWrapper
+//
+//  Created by Christopher Ballinger on 9/17/13.
+//  Copyright (c) 2013 OpenWatch, Inc. All rights reserved.
+//
+
+#import "FFmpegWrapper.h"
+
+@implementation FFmpegWrapper
+
+@end
